b62552727dc9aad5186b31fcb48945cddc1c7a65
akhramovich.anton <akhramovich.anton@gmail.com>
2025-05-28 18:30:02 +0300

    Added user-service

---
+++ microservices/user-service/app/routes.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from .models import db, User
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    if User.query.filter_by(username=data["username"]).first():
+        return jsonify({"msg": "Username already exists"}), 400
+    user = User(username=data["username"])
+    user.set_password(data["password"])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"msg": "User registered"}), 201
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data["username"]).first()
+    if user and user.check_password(data["password"]):
+        token = create_access_token(identity=user.id)
+        return jsonify(access_token=token), 200
+    return jsonify({"msg": "Bad credentials"}), 401
+
+@auth_bp.route("/me", methods=["GET"])
+@jwt_required()
+def me():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    return jsonify(id=user.id, username=user.username)
