3c45c3f87b7edb9dae1f41237ff6dd71e452378d
akhramovich.anton <akhramovich.anton@gmail.com>
2025-06-15 04:30:22 +0300

    Fixed path to db

--- microservices/analytics-service/main.py
+++ microservices/analytics-service/main.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-DB_HOST = os.getenv('DB_HOST', 'postgres')
+DB_HOST = os.getenv('DB_HOST', 'database')
 DB_PORT = os.getenv('DB_PORT', '5432')
 DB_NAME = os.getenv('DB_NAME', 'mini_market')
 DB_USER = os.getenv('DB_USER', 'user')
